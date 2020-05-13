79ed44213d5a14aacaf7226a53f99775ab3777b4
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-05-13 16:41:14 +0300

    add checkboxes

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,30 @@
-function init() {
-  $city = document.getElementById("city")
-
-  $city.addEventListener("input", (event) => {
-    localStorage.setItem("city", event.target.value)
-  });
+function checToLS(){
+  $checList = document.querySelector(input[type="checkbox"])
+  console.log(checList);
 }
 
+function init() {
+  const $city = document.getElementById("city")
+  const $btn = document.getElementById("done-btn")
+
+  if(localStorage.getItem('city')) {
+    const $doneInput = document.getElementById("done-input");
+    let city = localStorage.getItem("city");
+    $doneInput.innerHTML = `Ваш город — ${city}`
+  }
+
+  $city.addEventListener("input", (e) => {
+    localStorage.setItem("city", e.target.value)
+  });
+
+   $btn.addEventListener("click", () => {
+    checToLS()
+  });
+}
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
+
+  window.addEventListener('storage', function(e) {
+    console.log(e.target);
+  });
 });
